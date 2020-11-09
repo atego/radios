f0f461802403f3408f04d077a507e85e9289f4d6
atego <atego@users.noreply.github.com>
2020-11-09 10:45:40 +0100

    Update datosEmisoras.js

--- js/datosEmisoras.js
+++ js/datosEmisoras.js
@@ -61,7 +61,7 @@ var datosEmisoras = [
   },
   {
       "emisora": "Relaxing jazz",
-      "url": "https://stream-02-eu.relaxingjazz.com/stream/1/",
+      "url": "http://stream-02-eu.relaxingjazz.com/stream/1/",
       "logo": "./logos/relax.png"
   },
   {
